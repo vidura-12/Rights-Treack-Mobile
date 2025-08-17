d7a3cd77bea52e8ece554077d3dd3462de079716
vidura-12 <viduranirmal195@gmail.com>
2025-08-17 15:10:22 +0530

    Revert "Connected To FireBase"
    This reverts commit 76b440e5b7fdd354ee5e8c4552dd1a522dfed5db.

--- human_rights_tracker/android/build.gradle.kts
+++ human_rights_tracker/android/build.gradle.kts
@@ -19,25 +19,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("com.google.gms.google-services") // no version here in module build.gradle.kts
-    id("dev.flutter.flutter-gradle-plugin")
-}
-// Top-level build.gradle.kts
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("com.google.gms:google-services:4.4.3") // Add this here
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
-
--- human_rights_tracker/lib/features/ui/landing_page.dart
+++ human_rights_tracker/lib/features/ui/landing_page.dart
@@ -7,7 +7,7 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(5, 19, 56, 1),
+      backgroundColor: Colors.indigo[900],
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
@@ -30,7 +30,7 @@ class LandingPage extends StatelessWidget {
               ElevatedButton(
                 onPressed: () => Navigator.pushNamed(context, AppRoutes.login),
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                child: const Text('Login', style: TextStyle(color: Color.fromRGBO(5, 19, 56, 1),)),
+                child: const Text('Login', style: TextStyle(color: Colors.indigo)),
               ),
             ],
           ),
