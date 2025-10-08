bf0232ecd93d2898303e018688b4017852ecbd58
vidura-12 <viduranirmal195@gmail.com>
2025-10-08 23:35:54 +0530

    Fix Dark Theme

--- human_rights_tracker/lib/features/ui/case_list_page.dart
+++ human_rights_tracker/lib/features/ui/case_list_page.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
